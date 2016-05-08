4370d3a47624c87ad17e947cb9836a0e9c25a9b0
aspin <tsunami70875@gmail.com>
2016-05-08 17:02:04 -0500

    added migrations file

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,5 +1,6 @@
 import './../config.js';
 import './accounts-email.js';
+import './migrations.js';
 import './register-api.js';
 import './fixtures.js';
 import './migrations.js';
---
+++ imports/startup/server/migrations.js
@@ -0,0 +1,9 @@
+import { Meteor } from 'meteor/meteor';
+
+// 5/8/2015
+// for user groups schema changes
+
+// Meteor.users.find().forEach((user) => {
+//   const groups = user.groups;
+//
+// });
