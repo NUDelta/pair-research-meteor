cd7677a1b65d5d5b2f8318f8273508cdfd88a1ff
aspin <tsunami70875@gmail.com>
2016-06-04 11:33:02 -0500

    removed migration

--- imports/startup/server/migrations.js
+++ imports/startup/server/migrations.js
@@ -4,11 +4,11 @@ import { Groups } from '../../api/groups/groups.js';
 
 // 6/03/2016 nuking group info
 
-Groups.remove({});
-Meteor.users.update({},
-  {
-    $set: {
-      groups: []
-    }
-  }
-);
+// Groups.remove({});
+// Meteor.users.update({},
+//   {
+//     $set: {
+//       groups: []
+//     }
+//   }
+// );
