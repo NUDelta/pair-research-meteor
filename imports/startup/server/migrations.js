eb852d5c94dbc69d21c826e0d976100f9fcd8a6d
aspin <tsunami70875@gmail.com>
2016-07-11 12:08:20 -0500

    removing migrations

--- imports/startup/server/migrations.js
+++ imports/startup/server/migrations.js
@@ -4,14 +4,14 @@ import { Groups } from '../../api/groups/groups.js';
 
 // 7/11/2016 adding new group and role infrastructure
 
-Groups.remove({});
-Meteor.users.update({},
-  {
-    $set: {
-      groups: []
-    }
-  }
-);
+// Groups.remove({});
+// Meteor.users.update({},
+//   {
+//     $set: {
+//       groups: []
+//     }
+//   }
+// );
 
 // 6/03/2016 nuking group info
 
