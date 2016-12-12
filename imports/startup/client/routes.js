990fd93ffeecd8c15ddc90df1204349e7aebd682
aspin <tsunami70875@gmail.com>
2016-12-11 23:49:07 -0500

    fix #18 added analytics and yarn

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,5 +1,6 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+import { analytics } from "meteor/okgrow:analytics";
 
 import '../../ui/blaze-helpers.js';
 
