004db1119200126aa6ed731faaf63fd5b3965052
yawkity <sehmon3@gmail.com>
2017-05-22 15:11:30 -0500

    Beginnings of adding Python scripts to project

---
+++ imports/api/stats/methods.js
@@ -0,0 +1,36 @@
+import { ValidatedMethod } from 'meteor/mdg:validated-method';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+import { exec } from 'child_process';
+import { log } from '../logs';
+
+import { TEST_SCRIPT } from '../../startup/config.js';
+
+export const getStats = new ValidatedMethod({
+    name: 'stats.getStats',
+    validate: new SimpleSchema({
+        groupId: {
+            type: String,
+            regEx: SimpleSchema.RegEx.Id
+        }
+    }).validator(),
+    run({groupId}) {
+        console.log("Function called")
+        // Simulating lag for the pairing in progress spinner.
+        if (Meteor.isDevelopment && !this.isSimulation) {
+            console.log("Dev/Simulation branch ran")
+            log.debug(`Sleeping for ${DEV_OPTIONS.LATENCY} ms...`);
+            Meteor._sleepForMs(DEV_OPTIONS.LATENCY);
+        }
+
+        log.info(`running Python script at ${TEST_SCRIPT}`);
+        const data = "Yooooooooooooo";
+        console.log(groupId);
+        const cmd = `echo '${data}' | python ${TEST_SCRIPT}`;
+        console.log(cmd);
+        const response = JSON.parse(Meteor.wrapAsync(exec)(cmd));
+        console.log(response);
+        log.info(`script results: ${JSON.stringify(response)}`);
+
+        return response;
+    }
+});
---
+++ private/scripts/test.py
@@ -0,0 +1,5 @@
+sum = 0
+for i in xrange(20):
+    sum += i
+
+print sum
