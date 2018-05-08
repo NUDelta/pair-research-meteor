28cff1bbac0d4db618c6889af390925261b9be5c
Kapil Garg <kapil1garg@gmail.com>
2018-05-08 11:17:54 -0500

    WIP revamped email template

--- imports/startup/server/accounts-email.js
+++ imports/startup/server/accounts-email.js
@@ -13,12 +13,28 @@ Accounts.emailTemplates.enrollAccount.subject = (user) => {
   return `You're invited to join ${ invitedGroup.groupName } for Pair Research`;
 };
 
-Accounts.emailTemplates.enrollAccount.text = (user, url) => {
-  return `To join this pair research pool, just click the link below:
+Accounts.emailTemplates.enrollAccount.html = (user, url) => {
+  const invitedGroup = user.groups[0];
+  var dataContext = {
+    path: url,
+    group: invitedGroup.groupName
+  };
 
-${ url }
+  SSR.compileTemplate('htmlEmail', Assets.getText('html/email-invite.html'));
+  console.log(Assets.absoluteFilePath('img/delta.jpg'));
+  Template.htmlEmail.helpers({
+    delta: Assets.absoluteFilePath('img/delta.jpg'),
+    call_merge: Assets.absoluteFilePath('img/call_merge.png'),
+    flash_on: Assets.absoluteFilePath('img/flash_on.png'),
+    people: Assets.absoluteFilePath('img/people.png')
+  });
 
-If you've received multiple invitation emails, please be sure to click the link for the most recent one.`;
+  return SSR.render('htmlEmail', dataContext);
+//   return `To join this pair research pool, just click the link below:
+
+// ${ url }
+
+// If you've received multiple invitation emails, please be sure to click the link for the most recent one.`;
 };
 
 Accounts.emailTemplates.resetPassword.subject = (user) => {
